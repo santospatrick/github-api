574caf56fcef2cb50cc92950d381e45a3ca9538a
Patrick Santos <patrickspteixeira@gmail.com>
2019-10-26 14:44:41 -0300

    adiciona animacao e chamada da api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Container, Form, SubmitButton } from './styles';
+import api from '../../services/api';
 
 function Main() {
+    const [newRepo, setNewRepo] = useState('');
+    const [repos, setRepos] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    function handleInputChange(e) {
+        setNewRepo(e.target.value);
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        setLoading(true);
+        const response = await api.get(`/repos/${newRepo}`);
+
+        const data = {
+            name: response.data.full_name,
+        };
+
+        setRepos([...repos, data]);
+        setLoading(false);
+    }
+
     return (
         <Container>
             <h1>
@@ -10,13 +33,26 @@ function Main() {
                 Repositórios
             </h1>
 
-            <Form onSubmit={() => {}}>
-                <input type="text" placeholder="Adicionar repositório" />
+            <Form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Adicionar repositório"
+                    onChange={handleInputChange}
+                    value={newRepo}
+                />
 
-                <SubmitButton disabled>
-                    <FaPlus color="#fff" size={14} />
+                <SubmitButton loading={loading}>
+                    {loading ? (
+                        <FaSpinner color="#fff" size={14} />
+                    ) : (
+                        <FaPlus color="#fff" size={14} />
+                    )}
                 </SubmitButton>
             </Form>
+
+            {repos.map(item => (
+                <li key={item}>{item.name}</li>
+            ))}
         </Container>
     );
 }
